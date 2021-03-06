e216e285b767ba9e46333d9663df1e085663e14e
dkile <dkile532@naver.com>
2021-03-06 18:02:20 +0900

    todo-app optimization completed

--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -7,10 +7,11 @@ import {
 import cn from 'classnames';
 import '../scss/TodoListItem.scss';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
 
   return (
+    <div className = "TodoListItem-virtualized" style = {style}>
     <div className="TodoListItem">
       <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
         {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
@@ -21,7 +22,11 @@ const TodoListItem = ({ todo, onRemove, onToggle }) => {
         <MdRemoveCircleOutline />
       </div>
     </div>
+    </div>
   );
 };
 
-export default React.memo(TodoListItem);
+export default React.memo(
+  TodoListItem,
+  (prevProps, nextProps) => prevProps.todo === nextProps.todo,
+  );
